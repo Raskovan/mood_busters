343e3c425c21628c7f6eec6ff2e1aab4939f0f7f
Raskovan <alexey.katalkin@gmail.com>
2018-02-12 14:14:54 -0500

    Images ixing

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -18,8 +18,26 @@ class ImagesController < ApplicationController
     redirect_to image_path(@image)
   end
 
+  def edit
+    @image = Image.find_by(id: params[:id])
+  end
+
+  def update
+    @image = Image.find_by(id: params[:id])
+    @image.update(image_params)
+    redirect_to images_path
+  end
+
+  def destroy
+    @image = Image.find_by(id: params[:id])
+    @image.destroy
+    redirect_to images_path
+  end
+
+  private
+
   def image_params
-    params.require(:image).permit(:name)
+    params.require(:image).permit(:name, :mood_id)
   end
 
 end
